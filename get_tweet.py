b5c4f7f8010790f8c92220ff97a8297952421aa3
EC2 Default User <ec2-user@ip-172-31-83-195.ec2.internal>
2020-11-20 15:53:27 +0000

    Remove Twitter and Spoonacular functions

--- get_tweet.py
+++ get_tweet.py
@@ -1,4 +1,3 @@
-import tweepy
 import os
 import datetime
 from time import strftime
@@ -6,112 +5,38 @@ import flask
 from flask import Flask
 import random
 from flask import request
-import requests
 from os.path import join, dirname
 from dotenv import load_dotenv
+from model_tweet import Tweet
+from model_spoonacular import Spoonacular
 
-app = Flask(__name__)
+APP = Flask(__name__)
 
-#get basic details, id, title, prep time, serving, image, link
-def get_recipe(query):
-    dotenv_path2 = join(dirname(__file__), 'spoonacular.env')
-    load_dotenv(dotenv_path2)
+@APP.route('/', methods=["GET"])
+def index():
+    """ Get tweets and recipes """
     
-    spoonacular_key = os.environ['SPOONACULAR_KEY']
-    
-    url = "https://api.spoonacular.com/recipes/complexSearch?query={}&addRecipeInformation=true&apiKey={}".format(query, spoonacular_key)
-    response = requests.get(url)
-    json_body = response.json()
-    
-    totalResults = json_body["totalResults"]
-    drink = random.randint(0,totalResults-1)
-    
-    idNum = json_body["results"][drink]["id"]
-    title = json_body["results"][drink]["title"]
-    prep_time = json_body["results"][drink]["readyInMinutes"]
-    serving = json_body["results"][drink]["servings"]
-    image = json_body["results"][drink]["image"]
-    link = json_body["results"][drink]["sourceUrl"]
-    
-    steps_num = len(json_body["results"][drink]["analyzedInstructions"][0]["steps"])
-    steps = [json_body["results"][drink]["analyzedInstructions"][0]["steps"][i]["step"] for i in range(0,steps_num)]
-    
-    return idNum, title, prep_time, serving, image, link, steps, totalResults
- 
-#get ingredients using id, create a second url and request
-def get_ingredients(idNum):
-    dotenv_path2 = join(dirname(__file__), 'spoonacular.env')
-    load_dotenv(dotenv_path2)
-    
-    spoonacular_key = os.environ['SPOONACULAR_KEY']
-    
-    url2 = "https://api.spoonacular.com/recipes/{}/information?includeNutrition=false&apiKey={}".format(idNum, spoonacular_key)
-    response2 = requests.get(url2)
-    json_body2 = response2.json()
-    
-    ingredients_len = len(json_body2["extendedIngredients"])
-    ingredients = [json_body2["extendedIngredients"][i]["original"] for i in range(0, ingredients_len)]
-    
-    return ingredients
- 
-#get tweets from twitter
-def get_quotes(query):
-    
-    dotenv_path = join(dirname(__file__), 'tweet.env')
-    load_dotenv(dotenv_path)
-    
-    consumer_key = os.environ['KEY']
-    consumer_secret = os.environ['SECRET']
-    access_token = os.environ['TOKEN']
-    access_token_secret = os.environ['TOKEN_SECRET']
-    
-    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-    auth.set_access_token(access_token, access_token_secret)
-    api = tweepy.API(auth)
-  
-    #request tweets using keyword in full text and english
-    get_tweets = api.search(query, lang='en', tweet_mode='extended')
-    
-    #display 3 tweets
-    counter = 0
-    name_list = []
-    user_list = []
-    text_list = []
-    time_list = []
-    
-    for tweet in get_tweets:
-        #dont repeat the same user
-        if counter < 3 and tweet.user.screen_name not in user_list:
-            
-            name_list.append(tweet.user.name)
-            user_list.append(tweet.user.screen_name)
-            text_list.append(tweet.full_text)
-            time_list.append(tweet.created_at.strftime("%a, %d %b %Y  |  %H:%M"))
-            counter += 1
-            
-    return name_list, user_list, text_list, time_list
-
-
-@app.route('/', methods=["GET"])
-def get_tweet():
-    
-    #get tweets from search
     query = request.args.get("search")
+    tweet = Tweet(query)
+    spoonacular = Spoonacular(query)
     
-    #add a random drink in the beginning
     if query == None:
-        #add featured drinks to page
-        drink_list = ["margarita", "mojito", "moscow mule", "martini", "pina colada", "mint julep", "whiskey sour"]
+        drink_list = [
+            "margarita", 
+            "mojito", 
+            "moscow mule", 
+            "martini", 
+            "pina colada", 
+            "mint julep", 
+            "whiskey sour"]
         drink = random.choice(drink_list)
         query = drink
       
     try:    
-      #get details and ingredients of query
-      idNum, title, prep_time, serving, image,link, steps, totalResults = get_recipe(query)
-      ingredients = get_ingredients(idNum)
+      idNum, title, prep_time, serving, image,link, steps, totalResults = spoonacular.get_recipe(query)
+      ingredients = spoonacular.get_ingredients(idNum)
       
-      #get quotes based on query
-      name_list, user_list, text_list, time_list = get_quotes(query)
+      name_list, user_list, text_list, time_list = tweet.get_quotes(query)
     
     except ValueError:
       return flask.render_template("index.html", totalResults = 0, query = query)
@@ -134,7 +59,8 @@ def get_tweet():
         totalResults = totalResults
         )
 
-app.run(
+APP.run(
     port = int(os.getenv("PORT", 8080)),   
-    host = os.getenv("IP", "0.0.0.0")
+    host = os.getenv("IP", "0.0.0.0"),
+    debug = True
 )  
