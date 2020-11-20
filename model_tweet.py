8bcea1cc4deaf1a907d9e0dec293d2a612087667
EC2 Default User <ec2-user@ip-172-31-83-195.ec2.internal>
2020-11-20 15:21:18 +0000

    Create new file for Twitter api, including getting tweets

---
+++ model_tweet.py
@@ -0,0 +1,44 @@
+import tweepy
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
+
+class Tweet:
+    def __init__(self, query):
+        self.query = query
+
+    def get_quotes(self, query):
+        
+        dotenv_path = join(dirname(__file__), 'tweet.env')
+        load_dotenv(dotenv_path)
+        
+        consumer_key = os.environ['KEY']
+        consumer_secret = os.environ['SECRET']
+        access_token = os.environ['TOKEN']
+        access_token_secret = os.environ['TOKEN_SECRET']
+        
+        auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+        auth.set_access_token(access_token, access_token_secret)
+        api = tweepy.API(auth)
+      
+        #request tweets using keyword in full text and english
+        get_tweets = api.search(query, lang='en', tweet_mode='extended')
+        
+        #display 3 tweets
+        counter = 0
+        name_list = []
+        user_list = []
+        text_list = []
+        time_list = []
+        
+        for tweet in get_tweets:
+            #dont repeat the same user
+            if counter < 3 and tweet.user.screen_name not in user_list:
+                
+                name_list.append(tweet.user.name)
+                user_list.append(tweet.user.screen_name)
+                text_list.append(tweet.full_text)
+                time_list.append(tweet.created_at.strftime("%a, %d %b %Y  |  %H:%M"))
+                counter += 1
+                
+        return name_list, user_list, text_list, time_list
