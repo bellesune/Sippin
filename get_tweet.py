fe805d36db9d2a2f8687ff37fc4efa2d4a6fb290
EC2 Default User <ec2-user@ip-172-31-83-195.ec2.internal>
2020-11-21 01:48:31 +0000

    Sanitize the code

--- get_tweet.py
+++ get_tweet.py
@@ -1,3 +1,4 @@
+""" An app that displays recipes and related Twitter quotes """
 import os
 import flask
 from flask import Flask
@@ -8,56 +9,64 @@ from model_spoonacular import Spoonacular
 
 APP = Flask(__name__)
 
-@APP.route('/', methods=["GET"])
+
+@APP.route("/", methods=["GET"])
 def index():
     """ Get tweets and recipes """
-    
+
     query = request.args.get("search")
     tweet = Tweet(query)
     spoonacular = Spoonacular(query)
-    
-    if query == None:
+
+    if query is None:
         drink_list = [
-            "margarita", 
-            "mojito", 
-            "moscow mule", 
-            "martini", 
-            "pina colada", 
-            "mint julep", 
-            "whiskey sour"]
+            "margarita",
+            "mojito",
+            "moscow mule",
+            "martini",
+            "pina colada",
+            "mint julep",
+            "whiskey sour",
+        ]
         drink = random.choice(drink_list)
         query = drink
-      
-    try:    
-      idNum, title, prep_time, serving, image,link, steps, totalResults = spoonacular.get_recipe(query)
-      ingredients = spoonacular.get_ingredients(idNum)
-      
-      name_list, user_list, text_list, time_list = tweet.get_quotes(query)
-    
+
+    try:
+        (
+            id_num,
+            title,
+            prep_time,
+            serving,
+            image,
+            link,
+            steps,
+            total_results,
+        ) = spoonacular.get_recipe(query)
+        ingredients = spoonacular.get_ingredients(id_num)
+
+        name_list, user_list, text_list, time_list = tweet.get_quotes(query)
+
     except ValueError:
-      return flask.render_template("index.html", totalResults = 0, query = query)
-      
+        return flask.render_template("index.html", total_results=0, query=query)
+
     return flask.render_template(
         "index.html",
-        query = query,
-        name_list = name_list,
-        user_list = user_list,
-        text_list = text_list,
-        time_list = time_list,
-        name_len = len(name_list),
-        idNum = idNum,
-        title = title,
-        prep_time = prep_time,
-        serving = serving,
-        image = image,
-        steps = steps,
-        link = link,
-        ingredients = ingredients,
-        totalResults = totalResults
-        )
+        query=query,
+        name_list=name_list,
+        user_list=user_list,
+        text_list=text_list,
+        time_list=time_list,
+        name_len=len(name_list),
+        id_num=id_num,
+        title=title,
+        prep_time=prep_time,
+        serving=serving,
+        image=image,
+        steps=steps,
+        link=link,
+        ingredients=ingredients,
+        total_results=total_results,
+    )
 
-APP.run(
-    port = int(os.getenv("PORT", 8080)),   
-    host = os.getenv("IP", "0.0.0.0"),
-    debug = True
-)  
+
+APP.run(port=int(os.getenv("PORT", 8080)), host=os.getenv("IP", "0.0.0.0"), debug=True)
--- model_spoonacular.py
+++ model_spoonacular.py
@@ -1,56 +1,70 @@
+""" Use spoonacular api to get recipe info """
 import os
 import requests
 import random
 from os.path import join, dirname
 from dotenv import load_dotenv
 
+
 class Spoonacular:
     """ get basic details, id, title, prep time, serving, image, link """
-    
+
     def __init__(self, query):
         """ Initialize the query """
         self.query = query
-    
+
     def get_recipe(self, query):
         """ Get the basic info of recipe """
-        
-        dotenv_path2 = join(dirname(__file__), 'spoonacular.env')
+
+        dotenv_path2 = join(dirname(__file__), "spoonacular.env")
         load_dotenv(dotenv_path2)
-        
-        spoonacular_key = os.environ['SPOONACULAR_KEY']
-        
-        url = "https://api.spoonacular.com/recipes/complexSearch?query={}&addRecipeInformation=true&apiKey={}".format(query, spoonacular_key)
+
+        spoonacular_key = os.environ["SPOONACULAR_KEY"]
+
+        url = (
+            "https://api.spoonacular.com/recipes/complexSearch?query={}&"
+            "addRecipeInformation=true&apiKey={}".format(query, spoonacular_key)
+        )
         response = requests.get(url)
         json_body = response.json()
-        
-        totalResults = json_body["totalResults"]
-        drink = random.randint(0,totalResults-1)
-        
-        idNum = json_body["results"][drink]["id"]
+
+        total_results = json_body["totalResults"]
+        drink = random.randint(0, total_results - 1)
+
+        id_num = json_body["results"][drink]["id"]
         title = json_body["results"][drink]["title"]
         prep_time = json_body["results"][drink]["readyInMinutes"]
         serving = json_body["results"][drink]["servings"]
         image = json_body["results"][drink]["image"]
         link = json_body["results"][drink]["sourceUrl"]
-        
+
         steps_num = len(json_body["results"][drink]["analyzedInstructions"][0]["steps"])
-        steps = [json_body["results"][drink]["analyzedInstructions"][0]["steps"][i]["step"] for i in range(0,steps_num)]
-        
-        return idNum, title, prep_time, serving, image, link, steps, totalResults
-     
-    def get_ingredients(self, idNum):
+        steps = [
+            json_body["results"][drink]["analyzedInstructions"][0]["steps"][i]["step"]
+            for i in range(0, steps_num)
+        ]
+
+        return id_num, title, prep_time, serving, image, link, steps, total_results
+
+    def get_ingredients(self, id_num):
         """ Get ingredients using id, create a second url and request """
-        
-        dotenv_path2 = join(dirname(__file__), 'spoonacular.env')
+
+        dotenv_path2 = join(dirname(__file__), "spoonacular.env")
         load_dotenv(dotenv_path2)
-        
-        spoonacular_key = os.environ['SPOONACULAR_KEY']
-        
-        url2 = "https://api.spoonacular.com/recipes/{}/information?includeNutrition=false&apiKey={}".format(idNum, spoonacular_key)
+
+        spoonacular_key = os.environ["SPOONACULAR_KEY"]
+
+        url2 = (
+            "https://api.spoonacular.com/recipes/{}/information?"
+            "includeNutrition=false&apiKey={}".format(id_num, spoonacular_key)
+        )
         response2 = requests.get(url2)
         json_body2 = response2.json()
-        
+
         ingredients_len = len(json_body2["extendedIngredients"])
-        ingredients = [json_body2["extendedIngredients"][i]["original"] for i in range(0, ingredients_len)]
-        
+        ingredients = [
+            json_body2["extendedIngredients"][i]["original"]
+            for i in range(0, ingredients_len)
+        ]
+
         return ingredients
